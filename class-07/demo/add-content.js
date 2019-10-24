c5598138edf7c04fd9feb4e34ca9dcf145a2e37b
= <ajohnson764@gmail.com>
2019-10-24 09:29:35 -0700

    adding function code

---
+++ class-07/demo/add-content.js
@@ -0,0 +1,49 @@
+'use strict'
+
+function fullName(potato, cabbage){
+  return potato + ' ' + cabbage;
+}
+
+var firstName = prompt('What is your first name?');
+var lastName = prompt('what is your last name?');
+
+var full = fullName("Josie", "Cat"); // Josie Cat
+
+alert(full);
+
+// // var today = new Date();
+// var hourNow = prompt('What time is it?'); // 10
+// console.log('Amanda, you put in the number' + hourNow);
+
+// var favoriteColor = prompt('What is your favorite color?'); //rEd
+// var greeting;
+
+// var newColor = favoriteColor.toLowerCase(); // red
+
+// if(newColor === 'red'){
+//   var name = prompt('What is your name?');
+//   if(name === "amanda"){
+//     alert('Congratulations! You are correct!!!!');
+//   } else{
+//     alert('Congratulations, you are wrong.');
+//   }
+
+// } else{
+//   alert('Congratulations, you are wrong.');
+
+// }
+
+
+// if (hourNow > 18) {
+//   greeting = 'Good evening, Class!';
+// } 
+// // if 15 is greater than 12
+// else if (hourNow > 12) {
+//   greeting = 'Good afternoon, Class!';
+// } else if (hourNow >= 0) {
+//   greeting = 'Good morning!';
+// } else {
+//   greeting = 'Welcome!';
+// }
+
+// document.write('<h3>' + greeting + '</h3>');
